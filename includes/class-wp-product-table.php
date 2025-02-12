cfcdc267641808246607068a8fa878d578a4fc32
kallbuloso <kallbuloso@gmail.com>
2025-02-12 20:03:41 -0300

    Refactor product name column to remove button and use a simple text display

--- includes/class-wp-product-table.php
+++ includes/class-wp-product-table.php
@@ -58,12 +58,7 @@ class WP_Product_Table extends WP_List_Table {
 
 	public function column_default( $item, $column_name ) {
         if ( $column_name === 'name' ) {
-            return sprintf(
-                '%s <button class="button view-product" data-id="%d">%s</button>',
-                esc_html( $item['name'] ),
-                intval( $item['id'] ),
-                __( 'Visualizar', 'wp-product-listing' )
-            );
+            return esc_html( $item['name'] );
         }
         return isset( $item[ $column_name ] ) ? esc_html( $item[ $column_name ] ) : '';
     }
@@ -71,7 +66,14 @@ class WP_Product_Table extends WP_List_Table {
 	public function column_actions( $item ) {
 		$edit_url = admin_url( 'admin.php?page=wp-product-listing&action=edit&id=' . intval( $item['id'] ) );
 		$delete_url = wp_nonce_url( admin_url( 'admin-post.php?action=wp_product_delete&id=' . intval( $item['id'] ) ), 'wp_product_nonce' );
-		return '<a href="' . esc_url( $edit_url ) . '">' . __( 'Editar', 'wp-product-listing' ) . '</a> | ' .
+		$view_url = '#'; // Placeholder URL for the view action
+		$view_link = sprintf(
+			'<a href="%s" class="view-product" data-id="%d">%s</a>',
+			esc_url( $view_url ),
+			intval( $item['id'] ),
+			__( 'Visualizar', 'wp-product-listing' )
+		);
+		return $view_link . ' | <a href="' . esc_url( $edit_url ) . '">' . __( 'Editar', 'wp-product-listing' ) . '</a> | ' .
 			   '<a href="' . esc_url( $delete_url ) . '" onclick="return confirm(\'' . esc_js( __( 'Tem certeza que deseja excluir este produto?', 'wp-product-listing' ) ) . '\')">' . __( 'Excluir', 'wp-product-listing' ) . '</a>';
 	}
     
