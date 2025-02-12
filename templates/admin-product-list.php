e1ef3c6d492e2027801da696240226c78f4cd7b6
kallbuloso <kallbuloso@gmail.com>
2025-02-12 08:26:01 -0300

    Add CSV import form to product listing page

--- templates/admin-product-list.php
+++ templates/admin-product-list.php
@@ -20,7 +20,18 @@ $product_table->prepare_items();
 
 <div class="wrap">
 	<h1 class="wp-heading-inline"><?php _e( 'Lista de Produtos', 'wp-product-listing' ); ?></h1>
-	<a href="<?php echo admin_url( 'admin.php?page=wp-product-listing&action=add' ); ?>" class="page-title-action"><?php _e( 'Adicionar Novo', 'wp-product-listing' ); ?></a>
+	<div style="float: right; margin-top: 5px;">
+		<a href="<?php echo admin_url( 'admin.php?page=wp-product-listing&action=add' ); ?>" class="page-title-action"><?php _e( 'Adicionar Novo', 'wp-product-listing' ); ?></a>
+		<!-- Formulário de Importação CSV -->
+		<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'admin-post.php' ); ?>" style="display:inline-block; margin-left:10px;">
+			<input type="hidden" name="action" value="wp_product_import_csv">
+			<?php wp_nonce_field( 'wp_product_import_csv_nonce' ); ?>
+			<input type="file" name="csv_file" accept=".csv">
+			<input type="submit" class="button" value="<?php _e( 'Importar CSV', 'wp-product-listing' ); ?>">
+		</form>
+		<!-- Fim do formulário CSV -->
+	</div>
+	<div style="clear: both;"></div>
 	<hr class="wp-header-end">
 	<form method="post">
 		<?php $product_table->display(); ?>
