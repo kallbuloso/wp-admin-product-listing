ba1510cda92bca2ccef67980687e2086b1f7bbab
kallbuloso <kallbuloso@gmail.com>
2025-02-15 18:24:49 -0300

    Add bulk edit modal functionality and image selection in admin.js

--- assets/admin.js
+++ assets/admin.js
@@ -29,7 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     });
-    
 
     closeModal.addEventListener("click", function () {
         modal.style.display = "none";
@@ -58,5 +57,56 @@ document.addEventListener("DOMContentLoaded", function () {
         if (event.target === csvModal) {
             csvModal.style.display = 'none';
         }
+    });
+
+    // Modal de edição em massa
+    const openBulkEditBtn = document.getElementById('open-bulk-edit-modal');
+    const bulkEditModal = document.getElementById('wp-bulk-edit-modal');
+    const closeBulkEditBtn = document.getElementById('close-bulk-edit-modal');
+
+    openBulkEditBtn.addEventListener('click', function() {
+        const selectedProductIds = Array.from(document.querySelectorAll('input[name="product[]"]:checked')).map(input => input.value);
+        document.getElementById('selected-product-ids').value = selectedProductIds.join(',');
+        bulkEditModal.style.display = 'flex';
+    });
+
+    closeBulkEditBtn.addEventListener('click', function() {
+        bulkEditModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target === bulkEditModal) {
+            bulkEditModal.style.display = 'none';
+        }
+    });
+
+    // Coletar IDs dos produtos selecionados
+    const bulkEditForm = document.querySelector('#wp-bulk-edit-modal form');
+    bulkEditForm.addEventListener('submit', function() {
+        const selectedProductIds = Array.from(document.querySelectorAll('input[name="product[]"]:checked')).map(input => input.value);
+        document.getElementById('selected-product-ids').value = selectedProductIds.join(',');
+    });
+
+    // Seleção de imagem para edição em massa
+    const selectBulkImageBtn = document.getElementById('select_bulk_image');
+    const bulkPhotoUrlInput = document.getElementById('bulk_photo_url');
+
+    selectBulkImageBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        // Se já existir um frame aberto, usa ele
+        var frame = wp.media({
+            title: "Selecione uma Imagem",
+            library: { type: "image" }, // Filtra apenas imagens
+            multiple: false, // Apenas uma imagem por vez
+            button: { text: "Usar esta imagem" }
+        });
+
+        // Ao selecionar uma imagem
+        frame.on("select", function () {
+            var attachment = frame.state().get("selection").first().toJSON();
+            bulkPhotoUrlInput.value = attachment.url; // Coloca a URL no input
+        });
+
+        frame.open();
     });
 });
